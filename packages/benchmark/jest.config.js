d0b3c6c7327989ade23e53c33131e9f21943778f
Muhammad Arslan <arslan_mecom@yahoo.com>
2026-07-27 11:42:04 +0200

    added test coverage for benchmark

--- packages/benchmark/jest.config.js
+++ packages/benchmark/jest.config.js
@@ -5,16 +5,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/index.ts'],
-  moduleNameMapper: {
-    '^@hazeljs/eval$': '<rootDir>/../eval/src',
-    '^@hazeljs/agent$': '<rootDir>/../agent/src',
-  },
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 };
